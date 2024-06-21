8005272cb10ee2df3ac3536b6a4794ea2002c245
Bi-sh-aL <bsal.stha123@gmail.com>
2024-06-21 13:26:13 +0545

    changed background image

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -44,7 +44,7 @@ function App() {
     <div
             className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
             style={{
-                backgroundImage: `url('https://images.pexels.com/photos/259209/pexels-photo-259209.jpeg?auto=compress&cs=tinysrgb&w=600')`,
+                backgroundImage: `url('https://images.pexels.com/photos/4386366/pexels-photo-4386366.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
             }}
         >
             <div className="w-full">
